1ef82dae97d991a02d2c3784ba0413027f981a56
subtleasian <danielmson21@gmail.com>
2020-04-04 11:14:01 -0700

    added my-sql changes

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,18 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS sql3331101;
+CREATE DATABASE sql3331101;
+USE sql3331101;
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  username varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  PRIMARY KEY(id)
+);
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE userScore (
+  id int NOT NULL AUTO_INCREMENT,
+  animal_breed varchar(255) NOT NULL,
+  animal_name varchar(255) NOT NULL,
+  price int NOT NULL,
+  buyer_id int NOT NULL,
+  PRIMARY KEY(id) FOREIGN KEY(buyer_id) REFERENCES buyers(id)
+);
