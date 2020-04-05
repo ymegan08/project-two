d938f8cba040fd0d0ef4538b03a55ee286650f42
iamjink <jinijinjin@gmail.com>
2020-04-04 20:02:43 -0700

    description of app in readme added

---
+++ models/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Luger Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Le Pigeon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Animal Style. In-N-Out Burger', false);
